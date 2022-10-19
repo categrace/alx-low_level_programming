f668e5d42ca02195f80d86663cfa2229a87ab0aa
categrace <categrace0@gmail.com>
2022-10-19 03:17:34 -0700

    task 4

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,9 +1,24 @@
 #include "main.h"
 
 /**
+  *@c:an input character
   *_islower - check main.h
-  *print1 if c is lower case and 0 otherwise
+  *Description:usese _putchar function to print
+  *Return 1 if c is lower case and 0 otherwise
   */
 
 int _islower(int c)
-	
+{
+	char  i;
+	int islower = 0;
+
+	for (i = 'a'; i <= 'z'; i++)
+
+	{
+		if (i == c)
+		{
+			islower = 1;
+		}
+	}
+	return (islower);
+}
---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+  *_isalpha checks main.h
+  *@c: An input character
+  *Description: function returns 1 if character is 
+  *letter, lowercase or upercase
+  *Returns 1 otherwise 0.
+  */
+int _isalpha(int c)
+{
+	char islower, isupper;
+	int isletter = 0;
+
+
+	for (islower = 'a'; islower <= 'z'; islower++)
+	{
+		for (isupper = 'A'; isupper <= 'Z'; isupper++)
+		{
+
+			if (c == islower || c == isupper)
+			
+				isletter = 1;
+
+		}
+	}
+	return (isletter);
+}
